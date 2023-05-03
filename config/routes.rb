9cd6d64ced6c922bbd48330ba70642d7e4f89cca
Ryan <rrichardson610@gmail.com>
2023-05-03 10:35:19 -0700

    Big ass initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  namespace :admin do
+    resources :posts
+
+    root to: "posts#index"
+  end
+
+  resources :posts
+  root 'public#index'
+end
